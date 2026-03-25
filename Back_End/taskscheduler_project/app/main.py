41e87eb6a662344b791db8be6e157fde92b59eb1
bhavanavishali <bhavanakukku894@gmail.com>
2026-03-25 18:10:05 +0530

    main.py file routes order changeds

--- Back_End/taskscheduler_project/app/main.py
+++ Back_End/taskscheduler_project/app/main.py
@@ -1,25 +1,26 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routes.auth_route import router as auth_router
 import os
+
+from .routes.auth_route import router as auth_router
 from .routes.task_route import router as task_router
 
+app = FastAPI()
 
-app=FastAPI()
- 
+allowed_origins = os.getenv(
+    "ALLOWED_ORIGINS",
+    "http://localhost:5173"
+).split(",")
 
-
-origins = [
-    "http://localhost:5173",
-    "https://tasks-scheduler-cyan.vercel.app",
-]
+print("CORS origins:", allowed_origins)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=allowed_origins,
     allow_credentials=True,
-    allow_methods=["POST", "GET", "PUT", "DELETE", "OPTIONS","PATCH"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(auth_router)
-app.include_router(task_router, prefix="/api") 
+app.include_router(task_router, prefix="/api")
